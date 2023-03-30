1b29a5bd52904d874276dd211194207c65dfc418
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-03-30 16:10:30 -0300

    [feature] including email in authentication

--- src/Users/Users/Models/AuthenticateDto.cs
+++ src/Users/Users/Models/AuthenticateDto.cs
@@ -8,6 +8,9 @@ namespace Users.Models
     {
         [Required]
         public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
         [Required]
         public string Password { get; set; }
     }
