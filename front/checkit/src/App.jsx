0d891747b28a5f1446a73ed9ce46d6393b6499d0
feitosinha97 <89053915+feitosinha97@users.noreply.github.com>
2023-05-11 18:51:44 -0300

    salvando trabalho temporário

--- front/checkit/src/App.jsx
+++ front/checkit/src/App.jsx
@@ -1,52 +1,22 @@
-import { useEffect, useState } from "react"
-import { NewTodoForm } from "./NewTodoForm"
-import "./styles.css"
-import { TodoList } from "./TodoList"
+import React, { useState } from "react";
+import './App.css';
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
 
-export default function App() {
-  const [todos, setTodos] = useState(() => {
-    const localValue = localStorage.getItem("ITEMS")
-    if (localValue == null) return []
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
 
-    return JSON.parse(localValue)
-  })
-
-  useEffect(() => {
-    localStorage.setItem("ITEMS", JSON.stringify(todos))
-  }, [todos])
-
-  function addTodo(title) {
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { id: crypto.randomUUID(), title, completed: false },
-      ]
-    })
-  }
-
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, completed }
-        }
-
-        return todo
-      })
-    })
-  }
-
-  function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
   }
 
   return (
-    <>
-      <NewTodoForm onSubmit={addTodo} />
-      <h1 className="header">Todo List</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
-    </>
-  )
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
 }
+
+export default App;
