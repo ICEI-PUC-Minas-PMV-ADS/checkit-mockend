b1b5d2f9d098554838739ee5bb31313b7e140a72
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-05-13 15:25:09 -0300

    register

--- front/checkit/src/components/RegisterForm.jsx
+++ front/checkit/src/components/RegisterForm.jsx
@@ -1,54 +1,58 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { register } from "../Services/Api";
+import { register } from "../services/Api";
 
 export function RegisterForm() {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = await register(name, email, pass);
-        console.log(data);
-    }
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log(pass)
+    const data = await register(name, email, pass);
+    console.log(data);
+  };
 
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="new-item-form">
-                <div className="form-row">
-                    <h2>Register</h2>
-                    <label htmlFor="name">Full name</label>
-                    <input
-                        value={name}
-                        name="name"
-                        onChange={(e) => setName(e.target.value)}
-                        id="name-item"
-                        placeholder="full Name" />
-                    <label htmlFor="item">Email</label>
-                    <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="email"
-                        placeholder="youremail@gmail.com"
-                        id="email-item"
-                        name="email"
-                    />
-                    <label htmlFor="item">Password</label>
-                    <input
-                        value={pass}
-                        onChange={(e) => setPass(e.target.value)}
-                        type="password"
-                        placeholder="**************"
-                        id="pass- item"
-                        name="password"
-                    />
-                </div>
-                <button className="btn" type="submit">Register</button>
-            </form>
-            <Link to="/">
-                <button className="btn">Already have an account? Login here.</button>
-            </Link>
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <h2>Register</h2>
+          <label htmlFor="name">Full name</label>
+          <input
+            value={name}
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            id="name-item"
+            placeholder="full Name"
+          />
+          <label htmlFor="item">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="youremail@gmail.com"
+            id="email-item"
+            name="email"
+          />
+          <label htmlFor="item">Password</label>
+          <input
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type="password"
+            placeholder="**************"
+            id="pass-item"
+            name="password"
+          />
+        </div>
+        <button className="btn" type="submit">
+          Register
+        </button>
+      </form>
+      <Link to="/">
+        <button className="btn">Already have an account? Login here.</button>
+      </Link>
+    </>
+  );
 }
--- front/checkit/src/services/Api.js
+++ front/checkit/src/services/Api.js
@@ -1,22 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://localhost:5278/api',
-  timeout: 10000,
+  baseURL: "https://localhost:5278/api",
+  //timeout: 10000,
 });
 
 export const login = async (email, pass) => {
-  const response = await api.post('/login', { email, pass });
+  const response = await api.post("/login", { email, pass });
   return response.data;
 };
 
-export const register = async (name, email, pass) => {
-  const response = await api.post('/Users', { name, email, pass });
+export const register = async (name, email, pass, role = 1) => {
+  console.log(pass);
+  const response = await api.post("/Users", { name, email, pass, role });
   return response.data;
 };
 
 export const getTodos = async (token) => {
-  const response = await api.get('/todos', {
+  const response = await api.get("/todos", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
