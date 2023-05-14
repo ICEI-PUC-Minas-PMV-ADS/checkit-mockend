a49de386570f3e1802c563a7b6d39a2388bcf373
Julia Sartori <juliasartori@outlook.com.br>
2023-05-15 00:58:38 +0200

    project with tasks

--- front/checkit/src/components/ProjectList.jsx
+++ front/checkit/src/components/ProjectList.jsx
@@ -1,29 +1,28 @@
 import { useEffect, useState } from 'react';
-import { getAllProjects } from "../services/Api.js";
+import { getAllProjects, getTaskbyProject } from "../services/Api.js";
 
 
 function ProjectList() {
   const [projects, setProjects] = useState([]);
 
-
-  // const [projectsTeste] = [
-  //   { name: 'Projeto 1', key: 'A', updatedBy: 'Julia', dueDate: '2023-05-14T12:10:02.275Z', status: 'concluido' },
-  //   { name: 'Projeto 2', key: 'M', updatedBy: 'Ila', dueDate: '2023-06-14T12:10:02.275Z', status: 'concluido' },
-  //   { name: 'Projeto 3', key: 'P', updatedBy: 'Henry', dueDate: '2023-08-14T12:10:02.275Z', status: 'concluido' },
-  //   { name: 'Projeto 4', key: 'L', updatedBy: 'Lucia', dueDate: '2023-09-14T12:10:02.275Z', status: 'concluido' },
-  //   ]
-
-
   useEffect(() => {
-    getAllProjects()
-      .then((res) => {
-        console.log(res);
-        setProjects(res.data);
-      })
-      .catch((error) => console.log(error));
+    async function fetchData() {
+      try {
+        const projectResponse = await getAllProjects();
+        const projectsWithTasks = await Promise.all(projectResponse.map(async (project) => {
+          const taskResponse = await getTaskbyProject(project.id);
+          return { ...project, tasks: taskResponse };
+        }));
+        setProjects(projectsWithTasks);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
   }, []);
 
-    return projects;
+  return projects;
 }
 
 export default ProjectList;
